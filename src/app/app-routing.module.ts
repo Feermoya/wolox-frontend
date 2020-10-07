2cc79c0c32f4ff6109283f529d23ea1f3dbeb5b4
Fernando <fmoya97.fm@gmail.com>
2020-10-07 20:16:29 -0300

    add: twitter-icon, login-module, refactor: home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
+import { TechsComponent } from './components/techs/techs.component';
+import { LoginModule } from './login/login.module';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +16,12 @@ import { LoginComponent } from './login/login.component';
     FooterComponent,
     HomeComponent,
     NavbarComponent,
-    LoginComponent
+    TechsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
